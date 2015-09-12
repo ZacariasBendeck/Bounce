2fc6c44a7c580590bc8fd2247a414166e8529bb1
Zacarias Bendeck <zbendeck@gmail.com>
2015-09-12 04:28:37 -0600

    Bounce2 running, count is working just need to erase previous score draw

---
+++ paddlegame.py
@@ -0,0 +1,27 @@
+#https://www.youtube.com/watch?v=GXj746OVm3I
+
+import time
+
+print('Please enter your username.')
+
+name = raw_input("@>> ")
+
+
+username = 'Zack'
+password = ''
+
+if name == username:
+	print('Please Wait')
+	time.sleep(1.5)
+	print("")
+	print('Correct, Please enter your password')
+
+password = input
+
+if name != username):
+	print('Please wait')
+	time.sleep(1.5)
+	print('')
+	print('Incorrect, closing program')
+	time.sleep(1.5)
+	exit
